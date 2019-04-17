9e1de65e634367f8b06ee6ada78a9fbb976e4500
ElvisCarlosDuranDehe <ElvisCarlosDuranDehe@MIDL001939>
2019-04-17 14:14:55 -0500

    Fix Time

--- Exercise_11/src/exercise/exercise11.java
+++ Exercise_11/src/exercise/exercise11.java
@@ -7,18 +7,20 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class exercise11 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.gecko.driver", "C:/GeckoDriver/geckodriver.exe");
 		WebDriver driver = new FirefoxDriver();
         
 		//STEP 2
 		driver.get("http://toolsqa.com/automation-practice-switch-windows/");
+		Thread.sleep(4000);
 		//STEP 3
         driver.findElement(By.id("alert")).click();
         
         //STEP 4
         Alert windowAlert = driver.switchTo().alert();
+        Thread.sleep(4000);
       //STEP 5
         windowAlert.accept();
         driver.close();
